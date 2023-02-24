9efe35282393e75455c13c99e9e82d1ed82b0bfc
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-02-24 17:08:57 +0300

    add materializecss

--- src/App.vue
+++ src/App.vue
@@ -23,20 +23,8 @@ export default{
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   background-color: #100124;
 }
 
-nav {
-  padding: 20px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
